5e26d8fafcbf0b4b726258b3ef996aa47b6aa81f
Vladislav Bogdanov <vbogdanov2008@gmail.com>
2024-05-11 14:56:05 +1000

    'S1'

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using System;
+
+Console.WriteLine("Введите элементы массива через пробел:");
+string input = Console.ReadLine();
+string[] originalArray = input.Split(' ');
+
+Console.WriteLine("Новый массив из строк, длина которых меньше или равна 3 символам:");
