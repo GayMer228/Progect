0dbacafca9b687c6f77f11e35621737e8607e502
Vladislav Bogdanov <vbogdanov2008@gmail.com>
2024-05-11 14:57:39 +1000

    'S2'

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
 ﻿using System;
 
-Console.WriteLine("Введите элементы массива через пробел:");
-string input = Console.ReadLine();
-string[] originalArray = input.Split(' ');
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива через пробел:");
+        string input = Console.ReadLine();
+        string[] originalArray = input.Split(' ');
 
-Console.WriteLine("Новый массив из строк, длина которых меньше или равна 3 символам:");
+        string[] newArray = FilterArrayByLength(originalArray);
+
+        Console.WriteLine("Новый массив из строк, длина которых меньше или равна 3 символам:");
+        foreach (string str in newArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    private static string[] FilterArrayByLength(string[] originalArray)
+    {
+        throw new NotImplementedException();
+    }
+}
